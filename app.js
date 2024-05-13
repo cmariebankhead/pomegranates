f17d51f01a629536e58e992c7227392db71e59c3
cmariebankhead <162347513+cmariebankhead@users.noreply.github.com>
2024-05-13 00:00:11 -0500

    Update web pages
    add content to webpages

--- app.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Perform login validation here
-    alert('Login button clicked!');
-  }); 
